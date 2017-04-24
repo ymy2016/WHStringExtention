f46eace0294f7ad4b799c2a55be4ffbfd13a3a7e
ymy2016 <675784509@qq.com>
2017-04-24 14:55:51 +0800

    初始化Demo
    初始化Demo

---
+++ StringCutExtension/StringCutExtension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StringCutExtension
+//
+//  Created by 伟华 on 17/4/24.
+//  Copyright © 2017年 WH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
